cd77cb9ab54d8994b39e1f4b3d2455675b1b814e
Kibru <kibrussmenore@gmail.com>
2026-04-27 16:09:55 -0500

    prepare for deployment,  add environment variable support for API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
 const api      = axios.create({ baseURL: BASE_URL });
 
 export const getAllSchools     = (params = {}) => api.get("/api/schools",            { params }).then((r) => r.data);
